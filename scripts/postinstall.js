bc4dfb5ea829230c7f5df9b945cffd0a2d126563
Olcay Mete Ozdemir <hey@meteozdemir.com>
2022-11-14 16:33:44 +0300

    Add postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const configPath = path.join(process.env.INIT_CWD, '.stylelintrc.js');
+const ignorePath = path.join(process.env.INIT_CWD, '.stylelintignore');
+
+if (!fs.existsSync(configPath)) {
+	fs.writeFileSync(
+		configPath,
+		"module.exports = {\n\textends: [\n\t\t'@meteozdemir/stylelint-config-core',\n\t\t'@meteozdemir/stylelint-config-core/rules/prettier',\n\t],\n\trules: {},\n};\n",
+	);
+}
+
+if (!fs.existsSync(ignorePath)) {
+	fs.writeFileSync(ignorePath, 'node_modules\n');
+}
